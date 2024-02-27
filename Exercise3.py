2aa7730b1f0fc1be80983072247f69fb3cc0e5dc
jpfnice <jpfnice@gmail.com>
2024-02-27 12:04:28 +0100

    Add files via upload

---
+++ Exercise3.py
@@ -0,0 +1,18 @@
+"""
+Write a Python scripts that implements the “Guess the Number” game.
+
+The script will generate of a random integral number (use the module random) from 1 to 100, and ask you to guess it.
+
+The script will tell you if each guess is too high or too low.
+
+You win if you can guess the number within six tries.
+"""
+
+import random
+
+secret=random.randint(1,100) 
+# secret is the name of the variable
+# that holds the number to guess
+
+
+# To be continued ...
---
+++ module1.py
@@ -0,0 +1,12 @@
+import random
+import math
+
+result=math.sqrt(56)
+
+print(result)
+
+result=math.cos(34)
+
+print(result)
+
+print(random.randint(1,100))
