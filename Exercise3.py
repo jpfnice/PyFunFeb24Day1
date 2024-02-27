75bcf3b011c7610ad5d1fa23e4c98297bd6f994e
jpfnice <jpfnice@gmail.com>
2024-02-27 13:51:11 +0100

    Add files via upload

--- Exercise3.py
+++ Exercise3.py
@@ -10,9 +10,30 @@ You win if you can guess the number within six tries.
 
 import random
 
-secret=random.randint(1,100) 
+secret=random.randint(1,100)
+ 
 # secret is the name of the variable
 # that holds the number to guess
 
+print(secret) # Just for testing purpose ...
 
-# To be continued ...
+currentNumberOfAttempts=0
+
+while currentNumberOfAttempts < 6:
+
+    currentValue=input("Enter an int in the range [1,100]: ")
+    currentValue=int(currentValue)
+    
+    if currentValue == secret:
+        print ("Bingo ! You've found the secret number")
+        break
+    elif currentValue > secret:
+        print("Too large !")
+    elif currentValue < secret:
+        print("Too small !") 
+        
+    #currentNumberOfAttempts = currentNumberOfAttempts + 1
+    currentNumberOfAttempts += 1 # -= *= /=
+    
+if secret != currentValue:
+    print("The secret number was:", secret)
